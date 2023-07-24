62f9aba127b2c730799883fa28d30444c34d6b09
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2023-07-24 15:37:05 -0700

    move login form to correct place

--- src/features/auth/routes/login.tsx
+++ src/features/auth/routes/login.tsx
@@ -1,6 +1,6 @@
 
 import { useNavigate } from 'react-router-dom';
-import { LoginForm } from '../components/__tests__/login-form';
+import { LoginForm } from '../components/login-form';
 import { useUser } from 'hooks/use-user';
 import { useEffect } from 'react';
 
